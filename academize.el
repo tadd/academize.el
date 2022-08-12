a83e35af4abe32af8ffc54aa59c116bde5a84dca
Tadashi Saito <tad.a.digger@gmail.com>
2022-08-13 01:02:53 +0900

    We can just use a newer built-in feature.

--- academize.el
+++ academize.el
@@ -2,18 +2,10 @@
 ;;; まず最先端技術を使う宣言をします
 
 (defun academize ()
-  (defun mark-wrapper (f)
-    (push-mark (point))
-    (beginning-of-buffer)
-    (funcall f)
-    (pop-mark)
-    (goto-char (mark)))
-
   (defun replace-all (from to)
-    (mark-wrapper
-     (lambda ()
-       (while (search-forward from nil 'noerror)
-         (replace-match to)))))
+    (save-excursion
+      (while (search-forward from nil 'noerror)
+        (replace-match to))))
 
   (defun replace-all-list (l)
     (dolist (pair l)
